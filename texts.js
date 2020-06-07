b2f1de3c11f54985ca867ed21b0530e206eb8fd6
Graham Johnson <graham@commonwealth.im>
2020-06-07 19:17:11 -0400

    Purge texts.js

--- texts.js
+++
@@ -1,2 +0,0 @@
-// exports['Wagner: Reciprocity of Perspectives.js'] = require('./texts/Wagner 2018 Reciprocity of Perspectives.js'); 
-// exports['Conceptual Engineering.js'] = require('./texts/Conceptual Engineering.js'); 
